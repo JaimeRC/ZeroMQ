8873dd4af2e16e267c49c8662c8e98fb947f582e
JaimeRC <jaime.rc@icloud.com>
2018-06-24 23:03:48 +0200

    add Subscriber - PubSub

---
+++ src/PubSub/Subscriber/sub3.ts
@@ -0,0 +1,22 @@
+require('dotenv').config()
+import * as zmq from 'zeromq'
+
+const HOST: string = process.env.HOST,
+    PORT_PUB_SUB: string = process.env.PORT_PUB_SUB,
+    sub = zmq.socket('sub'),
+    ip: string = `tcp://${HOST}:${PORT_PUB_SUB}`,
+    channel: string = 'PubSub'
+
+export = {
+
+    conection(): any {
+        sub.connect(ip)
+        sub.subscribe(channel)
+    },
+
+    getMessage(): any {
+        sub.on('message', function (msg: Buffer) {
+            console.log('Subcrite3 recibido: ' + msg.toString())
+        })
+    }
+}
