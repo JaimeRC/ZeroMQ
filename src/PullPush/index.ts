eaf7443673764f3664eb2318726cd482d82ce94a
JaimeRC <jaime.rc@icloud.com>
2018-07-01 11:36:41 +0200

    fix

--- src/PullPush/index.ts
+++ src/PullPush/index.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 import push from './Producer'
 import pull from './Worker'
 
-export = (req: Request, res: Response) => {
+export = (req: Request, res: Response): void => {
 
     const { body } = req
 
