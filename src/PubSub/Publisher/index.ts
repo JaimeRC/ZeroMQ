6030e2b93b9dbad70d76445d598ee446c7932173
JaimeRC <jaime.rc@icloud.com>
2018-07-01 11:35:07 +0200

    add Publisher - PubSub

--- src/PubSub/Publisher/index.ts
+++ src/PubSub/Publisher/index.ts
@@ -3,17 +3,17 @@ import * as zmq from 'zeromq'
 
 const HOST: string = process.env.HOST,
     PORT_PUB_SUB: string = process.env.PORT_PUB_SUB,
-    pub = zmq.socket('pub'),
+    pub= zmq.socket('pub'),
     ip: string = `tcp://${HOST}:${PORT_PUB_SUB}`,
     channel: string = 'PubSub'
 
 export = {
 
-    conection(): any {
+    conection(): void {
         pub.bind(ip)
     },
 
-    sendMessage(msg: JSON): any {
+    sendMessage(msg: JSON): void {
         let query: string = JSON.stringify(msg)
         pub.send([channel, query])
     }
