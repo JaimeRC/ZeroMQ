97e4bb928d1c610536b717a1a141f50d64519eea
JaimeRC <jaime.rc@icloud.com>
2018-09-22 20:16:40 +0200

    fix in Broker

--- src/Proxy/Broker/index.ts
+++ src/Proxy/Broker/index.ts
@@ -1,22 +1,23 @@
 require('dotenv').config()
 import * as zmq from 'zeromq'
+import { Worker } from '../Worker/index'
 
-const { env: { HOST_BROKER, PORT_CLIENT_BROKER, PORT_WORKER_BROKER } } = process,
-    ipClient: string = `tcp://${HOST_BROKER}:${PORT_CLIENT_BROKER}`,
-    ipWorker: string = `tcp://${HOST_BROKER}:${PORT_WORKER_BROKER}`
+const { env: { HOST, PORT_CLIENT_BROKER, PORT_WORKER_BROKER } } = process,
+    ipBrokerClient: string = `tcp://${HOST}:${PORT_CLIENT_BROKER}`,
+    ipBrokerWorker: string = `tcp://${HOST}:${PORT_WORKER_BROKER}`,
+    idWorker: string = `worker_proxy_${process.pid}`
 
-let frontend: any,
-    backend: any
+let frontend= zmq.socket('router'),
+    backend= zmq.socket('router')
 
 let workers: Array<string> = []
 
 export = {
 
     loadFrontend(): void {
-        frontend = zmq.socket('router')
         frontend.identity = 'frontend_proxy' + process.pid
-        frontend.bind(ipClient, function (err: Error) {
-            if (err) throw err;
+        frontend.bind(ipBrokerClient, function (err: string) {
+            if (err) throw err
 
             console.log("Frontend en escucha: " + frontend.identity)
 
@@ -30,25 +31,26 @@ export = {
 
                     if (workers.length > 0) {
                         console.log("Frontend envia: " + query)
+
                         backend.send([workers.shift(), '', idClient, '', query])
                         clearInterval(interval)
                     }
 
                 }, 10)
-            });
-        });
+            })
+        })
     },
 
     loadBackend(): void {
         backend = zmq.socket('router')
         backend.identity = 'backend_proxy' + process.pid
-        backend.bind(ipWorker, function (err: Error): void {
-            if (err) throw err;
+        backend.bind(ipBrokerWorker, function (err: string): void {
+            if (err) throw err
 
             console.log("Backend en escucha: " + backend.identity)
 
             backend.on('message', function (...buffer: Array<Buffer>) {
-
+                console.log(buffer)
                 let idWorker: Buffer = buffer[0],
                     empty0: Buffer = buffer[1],
                     idClient: Buffer = buffer[2],
@@ -68,7 +70,9 @@ export = {
     },
 
     loadBroker(): void {
-        this.loadFrontend();
-        this.loadBackend();
+        this.loadFrontend()
+        this.loadBackend()
+
+        setTimeout(() => new Worker(idWorker, ipBrokerWorker).conection(), 10)
     }
 }  
