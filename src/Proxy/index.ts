d58f9018b5bdf4cce00a79c678f48d847ade36bf
JaimeRC <jaime.rc@icloud.com>
2018-09-22 20:17:19 +0200

    fix in Proxy

--- src/Proxy/index.ts
+++ src/Proxy/index.ts
@@ -1,29 +1,20 @@
 require('dotenv').config();
 import { Request, Response } from 'express'
 import { Client } from './Client/index'
-import broker = require('./Broker/index')
-import { Worker } from './Worker/index'
 
-const { env: { HOST, PORT_CLIENT_BROKER, PORT_WORKER_BROKER } } = process,
+const { env: { HOST, PORT_CLIENT_BROKER } } = process,
     idClient: string = `client_proxy_${process.pid}`,
-    ipClient: string = `tcp://${HOST}:${PORT_CLIENT_BROKER}`,
-    idWorker: string = `worker_proxy_${process.pid}`,
-    ipWorker: string = `tcp://${HOST}:${PORT_WORKER_BROKER}`
+    ipClient: string = `tcp://${HOST}:${PORT_CLIENT_BROKER}`
 
 export = (req: Request, res: Response): void => {
 
     const { body } = req;
 
-    broker.loadBroker()
+    let client = new Client(idClient, ipClient)
 
-    let client = new Client(idClient, ipClient, body)
-    let worker = new Worker(idWorker, ipWorker)
+    client.conection()
 
-    setTimeout((): void => {
-        worker.conection()
-        client.conection()
-        client.sendMessage()
-    }, 10)
+    client.sendMessage(body)
 
     client.getMessage()
         .then((data: JSON): void => {
